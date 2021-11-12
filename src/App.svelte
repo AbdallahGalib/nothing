444f99160c0bbf91eafc67cc2e4bb0ab973fc985
Abdallah Galib <abdallah.galib.wasi@gmail.com>
2021-11-12 18:00:07 +0600

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello Fellow {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 350px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: black;
+		text-transform: uppercase,lowercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: 1028px;
+		}
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'User Welcome to Money Management software made By our respectful creator and contributers: The Creator: Abdallah Galib, The Contributer: Galib Anwar'
+	}
+});
+export default app;
